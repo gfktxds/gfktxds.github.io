3a72b1fb4ecd1dc7d77b16373d7f731a0338f426
gfktxdspeach <103631587+gfktxdspeach@users.noreply.github.com>
2022-05-04 12:07:02 +0800

    Update time.js

--- static/plugin/clock-time/time.js
+++ static/plugin/clock-time/time.js
@@ -1,7 +1,7 @@
 var textAnimate = eval(Jscex.compile("async", function () 
 {
 	var together = new Date();
-	together.setFullYear(2022,03, 22); 			//时间年月日，注意月份是0-11
+	together.setFullYear(2022,03, 19); 			//时间年月日，注意月份是0-11
 	together.setHours(0);						//小时	
 	together.setMinutes(0);						//分钟
 	together.setSeconds(0);						//秒前一位
