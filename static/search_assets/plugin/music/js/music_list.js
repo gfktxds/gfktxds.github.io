8b99b32df4a01cf8defb262f5e8d7500e6eb177b
gfktxdspeach <103631587+gfktxdspeach@users.noreply.github.com>
2022-05-29 14:39:11 +0800

    Add files via upload

---
+++ static/search_assets/plugin/music/js/music_list.js
@@ -0,0 +1,11 @@
+var musicList = [{
+    title: "淅沥雨声",
+    artist: "Pattering rain",
+    mp3: "https://img-cdn2.yespik.com/sound/00/32/59/75/325975_ce5110162f896d1496e7c340226e6d1f.mp3",
+    poster: "https://img2.baidu.com/it/u=2522149780,2844876955&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=800"
+}, {
+    title: "Rainy Mood",
+    artist: "Rainy Mood",
+    mp3: "https://rainymood.com/audio1112/0.ogg",
+    poster: "https://rainymood.com/i/badge.jpg"
+}];
---
+++ static/search_assets/plugin/weather_rain/weather_style.js
@@ -0,0 +1,3 @@
+    var canvas_s = document.getElementById('myCanvas');
+    canvas_s.style.width = window.innerWidth + "px";
+    canvas_s.style.height = window.innerHeight + "px";
